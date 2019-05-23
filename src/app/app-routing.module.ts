f1706a63cca1b4a3bfa4adbc1e8e911e44bf248a
David McElhill <dmcelhill01@qub.ac.uk>
2019-05-23 18:33:50 +0100

    Tweaked routing for landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'tabs', pathMatch: 'full' },
+  { path: '', redirectTo: 'tabs/home', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule', canLoad: [AuthGuard] },
-  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule', canLoad: [AuthGuard] },
-  { path: 'edit-request', loadChildren: './requests/edit-request/edit-request.module#EditRequestPageModule' },
-  { path: 'view-request', loadChildren: './requests/view-request/view-request.module#ViewRequestPageModule' }
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule', canLoad: [AuthGuard] }
 ];
 
 @NgModule({
