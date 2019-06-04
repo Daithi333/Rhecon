88037fabafcb8ee300e2b4de1f36d6dcfc24854f
David McElhill <dmcelhill01@qub.ac.uk>
2019-06-04 18:32:34 +0100

    imporved requests page and service

--- src/app/requests/requests.page.ts
+++ src/app/requests/requests.page.ts
@@ -4,6 +4,11 @@ import { Subscription } from 'rxjs';
 
 import { RequestsService } from './requests.service';
 import { RequestData } from './request-data.model';
+import { Request } from './request.model';
+import { Patient } from '../patients/patient.model';
+import { Consultant } from '../consultants/consultant.model';
+import { PatientsService } from '../patients/patients.service';
+import { ConsultantsService } from '../consultants/consultants.service';
 
 @Component({
   selector: 'app-requests',
@@ -11,37 +16,82 @@ import { RequestData } from './request-data.model';
   styleUrls: ['./requests.page.scss'],
 })
 export class RequestsPage implements OnInit, OnDestroy {
-  requestData: RequestData[];
-  viewableRequests: RequestData[];
+  requestData: RequestData[] = [];
+  viewableRequests: RequestData[] = [];
   isLoading = false;
   private requestSub: Subscription;
+  private patientSub: Subscription;
+  private consultantSub: Subscription;
   private currentSegment = 'active';
 
   constructor(
-    private requestsService: RequestsService
+    private requestsService: RequestsService,
+    private patientsService: PatientsService,
+    private consultantsService: ConsultantsService
   ) { }
 
   ngOnInit() {
-    this.isLoading = true;
-    this.requestSub = this.requestsService.getRequestsWithPatientAndConsultant()
-      .subscribe(requestData => {
-        this.requestData = requestData;
+    // this.isLoading = true;
+    // this.requestSub = this.requestsService.getRequestsWithPatientAndConsultant()
+    //   .subscribe(requestData => {
+    //     this.requestData = requestData;
 
-        if (this.currentSegment === 'active') {
-          this.viewableRequests = this.requestData.filter(
-            rd => rd.requestActive === true
-          );
-        } else {
-          this.viewableRequests = this.requestData.filter(
-            rd => rd.requestActive === false
-          );
+    //     if (this.currentSegment === 'active') {
+    //       this.viewableRequests = this.requestData.filter(
+    //         rd => rd.requestActive === true
+    //       );
+    //     } else {
+    //       this.viewableRequests = this.requestData.filter(
+    //         rd => rd.requestActive === false
+    //       );
+    //     }
+    //     this.isLoading = false;
+    // });
+    let requests: Request[];
+    this.requestSub = this.requestsService.requests
+      .subscribe(reqs => {
+        requests = reqs;
+        for (const key in requests) {
+          if (requests.hasOwnProperty(key)) {
+            let patient: Patient;
+            let consultant: Consultant;
+            this.patientSub = this.patientsService.getPatient(requests[key].patientId)
+              .subscribe(pat => {
+                patient = pat;
+                this.consultantSub = this.consultantsService.getConsultant(requests[key].consultantId)
+                  .subscribe(cons => {
+                    consultant = cons;
+                    this.requestData.push(
+                      new RequestData(
+                        requests[key].id,
+                        requests[key].title,
+                        requests[key].requestorId,
+                        patient,
+                        consultant,
+                        requests[key].notes,
+                        requests[key].requestActive,
+                        requests[key].createdOn,
+                        requests[key].lastUpdated
+                      )
+                    );
+                    if (this.currentSegment === 'active') {
+                      this.viewableRequests = this.requestData.filter(
+                          rd => rd.requestActive === true
+                      );
+                    } else {
+                      this.viewableRequests = this.requestData.filter(
+                        rd => rd.requestActive === false
+                      );
+                    }
+                  });
+              });
+          }
         }
-        this.isLoading = false;
-    });
-
+      });
   }
 
   ionViewWillEnter() {
+    //  TODO - write logic for when request is edited and router redirects back here
   }
 
   onSegmentToggle(event: CustomEvent<SegmentChangeEventDetail>) {
@@ -61,6 +111,12 @@ export class RequestsPage implements OnInit, OnDestroy {
   ngOnDestroy() {
     if (this.requestSub) {
       this.requestSub.unsubscribe();
+    }
+    if (this.patientSub) {
+      this.patientSub.unsubscribe();
+    }
+    if (this.consultantSub) {
+      this.consultantSub.unsubscribe();
     }
   }
 
--- src/app/requests/requests.service.ts
+++ src/app/requests/requests.service.ts
@@ -66,52 +66,50 @@ export class RequestsService {
 
   constructor(
     private authService: AuthService,
-    private httpClient: HttpClient,
-    private patientsService: PatientsService,
-    private consultantsService: ConsultantsService
+    private httpClient: HttpClient
   ) {}
 
   get requests() {
     return this._requests.asObservable();
   }
 
-  getRequestsWithPatientAndConsultant() {
-    return this._requests.asObservable()
-      .pipe(
-        map(requests => {
-          const requestsWithPatientAndConsultant: RequestData[] = [];
-          for (const key in requests) {
-            if (requests.hasOwnProperty(key)) {
-              let patient: Patient;
-              let consultant: Consultant;
-              this.patientsService.getPatient(requests[key].patientId)
-                .subscribe(pat => {
-                  patient = pat;
-                  this.consultantsService.getConsultant(requests[key].consultantId)
-                    .subscribe(cons => {
-                      consultant = cons;
-                      requestsWithPatientAndConsultant.push(
-                        new RequestData(
-                          requests[key].id,
-                          requests[key].title,
-                          requests[key].requestorId,
-                          patient,
-                          consultant,
-                          requests[key].notes,
-                          requests[key].requestActive,
-                          requests[key].createdOn,
-                          requests[key].lastUpdated
-                        )
-                     );
-                    });
-                });
-            }
-          }
-          // console.log(requestsWithPatientAndConsultant);
-          return requestsWithPatientAndConsultant;
-        })
-      );
-  }
+  // getRequestsWithPatientAndConsultant() {
+  //   return this._requests.asObservable()
+  //     .pipe(
+  //       map(requests => {
+  //         const requestsWithPatientAndConsultant: RequestData[] = [];
+  //         for (const key in requests) {
+  //           if (requests.hasOwnProperty(key)) {
+  //             let patient: Patient;
+  //             let consultant: Consultant;
+  //             this.patientsService.getPatient(requests[key].patientId)
+  //               .subscribe(pat => {
+  //                 patient = pat;
+  //                 this.consultantsService.getConsultant(requests[key].consultantId)
+  //                   .subscribe(cons => {
+  //                     consultant = cons;
+  //                     requestsWithPatientAndConsultant.push(
+  //                       new RequestData(
+  //                         requests[key].id,
+  //                         requests[key].title,
+  //                         requests[key].requestorId,
+  //                         patient,
+  //                         consultant,
+  //                         requests[key].notes,
+  //                         requests[key].requestActive,
+  //                         requests[key].createdOn,
+  //                         requests[key].lastUpdated
+  //                       )
+  //                    );
+  //                   });
+  //               });
+  //           }
+  //         }
+  //         // console.log(requestsWithPatientAndConsultant);
+  //         return requestsWithPatientAndConsultant;
+  //       })
+  //     );
+  // }
 
   getRequest(id: number) {
     return this._requests.pipe(take(1),
@@ -179,7 +177,4 @@ export class RequestsService {
 
   }
 
-  deleteRequest() {
-
-  }
 }
