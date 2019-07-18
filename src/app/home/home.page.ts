f62c07f063ef5f727ee28644a11f4b9697981253
David McElhill <dmcelhill01@qub.ac.uk>
2019-07-18 01:45:55 +0100

    home page tweak

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Profile } from '../profile/profile.model';
+import { ProfileService } from '../profile/profile.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  profile: Profile;
+  profileSub: Subscription;
+  isLoading = false;
 
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.profileService.fetchProfile()
+      .subscribe(profile => {
+      this.profile = profile;
+      this.isLoading = false;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.profileSub) {
+      this.profileSub.unsubscribe();
+    }
+  }
 }
