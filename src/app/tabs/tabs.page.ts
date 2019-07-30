b4be2c28dc5d4ecea5f33db80af077588e2641f4
David McElhill <dmcelhill01@qub.ac.uk>
2019-07-30 14:07:29 +0100

    hid patients from consultants

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
 })
-export class TabsPage implements OnInit {
+export class TabsPage implements OnInit, OnDestroy {
+  userType: string;
+  userTypeSub: Subscription;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.userTypeSub = this.authService.userType.subscribe(userType => {
+      this.userType = userType;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.userTypeSub) {
+      this.userTypeSub.unsubscribe();
+    }
   }
 
 }
