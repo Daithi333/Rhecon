c19de38c1540e6ffa1cf7f8cd5e1d0e04f60531a
David McElhill <dmcelhill01@qub.ac.uk>
2019-07-30 19:54:53 +0100

    cosmetic

--- php_rest_rhecon/api/user-type/read.php
+++ php_rest_rhecon/api/user-type/read.php
@@ -1,4 +1,14 @@
-<?php  
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: Authorization, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    exit();
+  }
+  
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
